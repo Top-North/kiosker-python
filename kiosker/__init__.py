57d8358510b162b93948f5c6079bc7076c6e38e9
Martin Claesson <claeysson@gmail.com>
2026-02-26 19:10:39 +0100

    Update import statement in __init__.py to include KioskerException

--- kiosker/__init__.py
+++ kiosker/__init__.py
@@ -1,3 +1,3 @@
 from .api import KioskerAPI
 from .data import Status, Result, Blackout, ScreensaverState
-from .exceptions import ConnectionError, TLSVerificationError, AuthenticationError, IPAuthenticationError, BadRequestError, PingError
+from .exceptions import KioskerException, ConnectionError, TLSVerificationError, AuthenticationError, IPAuthenticationError, BadRequestError, PingError
