081c213c261412e2bc63f52b1bf14ac628020a6c
Martin Claesson <claeysson@gmail.com>
2026-02-26 19:09:41 +0100

    Update version to 1.2.9 and import exceptions in __init__.py

--- kiosker/__init__.py
+++ kiosker/__init__.py
@@ -1,2 +1,3 @@
 from .api import KioskerAPI
 from .data import Status, Result, Blackout, ScreensaverState
+from .exceptions import ConnectionError, TLSVerificationError, AuthenticationError, IPAuthenticationError, BadRequestError, PingError
